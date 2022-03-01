f24a65a7af918c445673db192ce831376847b284
Roshana Perera <mkroshana@gmail.com>
2022-03-01 20:52:11 +0530

    Initial commit. Only work properly for lower numbers

---
+++ Vicky_The_Frog/Vicky.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Vicky
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter distance : ");
+        Scanner in = new Scanner(System.in);
+        int distance = in.nextInt();
+
+        int fiveM = 0;
+        int threeM = 0;
+        int oneM = 0;
+
+        while (distance >= 5)
+        {
+            fiveM = fiveM + 1;
+            distance = distance - 5;
+        }
+
+        while (distance >= 3)
+        {
+            threeM = threeM + 2;
+            distance = distance - 3;
+        }
+
+        while (distance >= 1)
+        {
+            oneM = oneM + 3;
+            distance = distance - 1;
+        }
+
+        System.out.println(fiveM + threeM);
+    }
+}
