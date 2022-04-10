2beccbc3bdc61c54777e4d7627b7f26bb8fa5ead
Roshana Perera <mkroshana@gmail.com>
2022-04-10 21:22:55 +0530

    Initial DFS commit

---
+++ DFS/dfs/src/main/java/com/krishtraining/App.java
@@ -0,0 +1,13 @@
+package com.krishtraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
