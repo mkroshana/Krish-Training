1853abf39757b1a9709a127894c0ed1919f356a1
Roshana Perera <mkroshana@gmail.com>
2022-04-10 21:26:39 +0530

    BFS inputs modified and added an image

--- BFS/bfs/src/main/java/com/krishtraining/App.java
+++ BFS/bfs/src/main/java/com/krishtraining/App.java
@@ -57,17 +57,17 @@ public class App
 
   public static void main(String[] args)
   {
-    App g = new App(4);
+    App g = new App(7);
 
     g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
+    g.addEdge(0, 2);                  
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 6);
+    g.addEdge(4, 5);
 
     System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
 
-    g.BFS(2);
+    g.BFS(0);
   }
 }
